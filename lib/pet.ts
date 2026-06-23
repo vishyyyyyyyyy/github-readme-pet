5e2cc5dc94cef0646622d1b8cfa9d9cbf8584916
vishyyyyyyyyy <217375979+vishyyyyyyyyy@users.noreply.github.com>
2026-06-23 12:35:13 -0500

    Update pet.ts

--- lib/pet.ts
+++ lib/pet.ts
@@ -1,13 +1,16 @@
+  import { ASSETS } from "@/lib/assets";
+
+
 export function getPetState(commits: number) {
   const base = "https://github-readme-pet-yw8h.vercel.app";
 
   if (commits === 0) {
     return {
-      asset: `${base}/assets/sleeping_cat.png`,
+      asset: `${base}${ASSETS.sleepingCat}`,
     };
   }
 
   return {
-    asset: `${base}/assets/awake_cat.png`,
+    asset: `${base}${ASSETS.awakeCat}`,
   };
 }
