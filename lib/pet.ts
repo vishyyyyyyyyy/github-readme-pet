cd7ab4ad59b4f72c588f77fa0fc1fc961de08f4c
vishyyyyyyyyy <217375979+vishyyyyyyyyy@users.noreply.github.com>
2026-06-25 18:56:08 -0500

    remove base link asset for cat

--- lib/pet.ts
+++ lib/pet.ts
@@ -2,15 +2,13 @@
 
 
 export function getPetState(commits: number) {
-  const base = "https://github-readme-pet-yw8h.vercel.app";
-
   if (commits === 0) {
     return {
-      asset: `${base}${ASSETS.sleepingCat}`,
+        asset: ASSETS.sleepingCat,
     };
   }
 
   return {
-    asset: `${base}${ASSETS.awakeCat}`,
+      asset: ASSETS.awakeCat,
   };
 }
